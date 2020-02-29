be0cc3a5ca117222209e69e7b6393a272ceadd32
MortyraSky <gornii2014@gmail.com>
2020-02-29 21:59:22 +0300

    init: start homework Typical Arrays Problems

--- src/index.js
+++ src/index.js
@@ -1,12 +1,34 @@
 
 exports.min = function min (array) {
+  array = array || [];
+  if(array.length != 0) {
+    array.sort(function(a, b) {
+      return a - b;
+    });
+    return array[0];
+  }
   return 0;
 }
 
 exports.max = function max (array) {
+  array = array || [];
+  if(array.length != 0) {
+    array.sort(function(a, b) {
+      return a - b;
+    }).reverse();
+    return array[0];
+  }
   return 0;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  array = array || [];
+  if(array.length != 0) {
+    let sum = 0;
+    array.forEach(element => 
+        sum += element
+    );
+  return sum / array.length;
+}
+return 0;
 }
